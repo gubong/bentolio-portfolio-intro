d262b20efd5770769b6e3883745405b6c2c736e1
gubong <105339056+gubong@users.noreply.github.com>
2025-07-22 15:37:44 +0900

    Add App component to render IntroSection

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import IntroSection from "./components/IntroSection";
+
+function App() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100vh", background: "#eee" }}>
+      <IntroSection />
+    </div>
+  );
+}
+
+export default App;
